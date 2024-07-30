459808d1688398b21ca3be47068afb01976c0c26
navneet <nav700neet@gmail.com>
2024-07-30 23:57:19 +0530

    ui

--- app/src/main/java/com/android/ai/aitutor/presentation/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/android/ai/aitutor/presentation/ui/screens/ChatScreen.kt
@@ -1,10 +1,80 @@
 package com.android.ai.aitutor.presentation.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun ChatScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Chat")
+                }
+            )
+        },
+        bottomBar = {
+            var text by remember { mutableStateOf("") }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .weight(1f),
+                    shape = RoundedCornerShape(50),
+                    value = text,
+                    onValueChange = {
+                        text = it
+                    },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
 
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.AutoMirrored.Filled.Send, contentDescription = "send")
+                }
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+        ) {
+
+        }
+    }
 }
