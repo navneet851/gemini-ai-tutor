95d59f78d522c88401a5f5505240d894d958592b
navneet <nav700neet@gmail.com>
2024-07-31 00:40:05 +0530

    ui

--- app/src/main/java/com/android/ai/aitutor/App.kt
+++ app/src/main/java/com/android/ai/aitutor/App.kt
@@ -10,10 +10,12 @@ import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.android.ai.aitutor.presentation.ui.navigation.BottomBar
 import com.android.ai.aitutor.presentation.ui.navigation.MyNavHost
 import com.android.ai.aitutor.presentation.ui.navigation.NavigationRailBar
+import com.android.ai.aitutor.presentation.ui.navigation.Routes
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -23,11 +25,12 @@ fun App(mainActivity: MainActivity) {
     val navController = rememberNavController()
     val windowClass = calculateWindowSizeClass(activity = mainActivity)
     val showNavigationRail = windowClass.widthSizeClass != WindowWidthSizeClass.Compact
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
 
     Scaffold(
         bottomBar = {
-            if (!showNavigationRail)
+            if (!showNavigationRail && currentRoute != Routes.Chat.route)
                 BottomBar(navController)
         }
     ) {
--- app/src/main/java/com/android/ai/aitutor/presentation/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/android/ai/aitutor/presentation/ui/screens/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.android.ai.aitutor.presentation.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -26,9 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.ai.aitutor.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -37,6 +43,13 @@ fun ChatScreen() {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {
+
+                    }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Menu")
+                    }
+                },
                 title = {
                     Text(text = "Chat")
                 }
@@ -46,7 +59,6 @@ fun ChatScreen() {
             var text by remember { mutableStateOf("") }
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .padding(10.dp)
                     .fillMaxWidth()
@@ -59,12 +71,31 @@ fun ChatScreen() {
                     onValueChange = {
                         text = it
                     },
+                    trailingIcon = {
+                        Row {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(painter = painterResource(id = R.drawable.outline_photo_camera_24), contentDescription = "camera")
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(painter = painterResource(id = R.drawable.baseline_photo_library_24), contentDescription = "send")
+                            }
+                        }
+
+                    },
                     colors = TextFieldDefaults.colors(
-                        unfocusedIndicatorColor = Color.Transparent
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
                     )
                 )
 
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .clip(RoundedCornerShape(50))
+                        .background(Color(0xFF8B64F8))
+                        ,
+                    onClick = { /*TODO*/ }
+                ) {
                     Icon(imageVector = Icons.AutoMirrored.Filled.Send, contentDescription = "send")
                 }
             }
